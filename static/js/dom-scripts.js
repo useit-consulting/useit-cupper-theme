bcc26632eeeb07831c7a71a802c87b211b215217
Tommy Feldt <tommyfeldt@gmail.com>
2020-09-18 11:50:27 +0200

    Add support for copy buttons

--- static/js/dom-scripts.js
+++ static/js/dom-scripts.js
@@ -160,3 +160,14 @@ function WidthChange(mq) {
     }
   });
 }());
+
+function copyRichText(text) {
+  const listener = function(ev) {
+    ev.preventDefault();
+    ev.clipboardData.setData('text/html', text);
+    ev.clipboardData.setData('text/plain', text);
+  };
+  document.addEventListener('copy', listener);
+  document.execCommand('copy');
+  document.removeEventListener('copy', listener);
+}
